9378b580b7051f7100362218c40756e3d06001f6
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-24 22:26:42 +0545

    added logo comp

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return (
+        <h2 className="flex items-center gap-.5 flex-col font-semibold">Music
+
+            <span className="">NFT Platform</span></h2>
+    )
+}
+
+export default Logo
